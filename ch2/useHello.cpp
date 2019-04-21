f46b88cd7bd106ee667cc1526076cdc6a14701bc
wanglei-vm <991853430@qq.com>
2019-04-21 14:09:21 +0800

    ch2 cmake基本使用

---
+++ ch2/useHello.cpp
@@ -0,0 +1,12 @@
+#include "libHelloSLAM.h"
+#include <iostream>
+// 使用 libHelloSLAM.h 中的 printHello() 函数
+int main( int argc, char** argv )
+{
+    printHello();
+    int a=0;
+    int n=1;
+    a=n+1;
+    std::cout<<a<<std::endl;
+    //return 0;
+}
